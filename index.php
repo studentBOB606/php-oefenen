621748e6a9273e0466926b7d6f0a6f2b70ed3d20
studentBOB606 <9025052@student.zadkine.nl>
2025-03-07 10:16:18 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<img class="image-main" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/885bac5d-2870-43ca-937f-2effc8a6307a/dfrzzh4-efc52160-9d56-438e-b489-82859d2ec808.png/v1/fill/w_1280,h_1280,q_80,strp/oni_mask_by_mslisko_dfrzzh4-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTI4MCIsInBhdGgiOiJcL2ZcLzg4NWJhYzVkLTI4NzAtNDNjYS05MzdmLTJlZmZjOGE2MzA3YVwvZGZyenpoNC1lZmM1MjE2MC05ZDU2LTQzOGUtYjQ4OS04Mjg1OWQyZWM4MDgucG5nIiwid2lkdGgiOiI8PTEyODAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.jrG2vSNGteLVvpVz9zkg6Suw5ICbgNomkT7xEeVpOxk" alt="img-main">
+<img class="img-profile" src="https://media.tenor.com/hDtIHRfhvBAAAAAe/ghost-pixelart.png" alt="Ghost Profile">
+
+<p class="main-sub">Welkom op mijn pagina! Ik ben <span>Bob,</span> een aankomende software developer & student die nieuwe ervaring en innovatie pushen in Rotterdam, NL.</p>
+
+<div class="nav-menu">
+    <a href="index.html" class="chung">Chung.</a>
+    <a href="experience.html" class="experience">Experience</a>
+    <a href="craft.html" class="craft">Craft</a>
+    <a href="about.html" class="about">About</a>
+</div>
+
+<a href=""><img class="settings" src="https://pnghq.com/wp-content/uploads/pnghq.com-settings-icon-symbol-prem-1.png" alt="settings" style="filter: invert(100%) sepia(0%) saturate(2000%) hue-rotate(255deg) brightness(100%) contrast(90%)"></a>
+
+<a target="_blank" href="Reach%20Out!%20(contact).html" class="reach-out">Reach Out!</a>
+<a target="_blank" href="https://github.com/studentBOB606" class="github"><img src="https://img.icons8.com/m_sharp/200/FFFFFF/github.png" alt="github" style="width:44px; height:44px;"></a>
+<a target="_blank" href="#" class="linkedin"><img src="https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/linkedin-app-white-icon.png" alt="github" style="width:38px; height:38px;"></a>
+
+localhost/php-oefenen
+<?php
